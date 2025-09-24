e57dfda39e6b91f8536e2aacf0bc37f17636bfa6
3dr-zzZ <508874905@qq.com>
2025-09-23 23:31:57 -0400

    moved summary related functions to a new file summary.py

--- main.py
+++ main.py
@@ -2,93 +2,68 @@ import random
 import time
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 from banner import Banner
 from special_banner import SpecialBanner
 from limited_banner import LimitedBanner
 from standard_banner import StandardBanner
+from summary import summarize
 from tqdm import trange
 
-def export_to_csv(new_column: list, column_name: str, file_name: str = "output.csv") -> None:
+
+############### Basic Setup ###############
+random.seed(123)
+banner_types = ["SpecialBanner", "LimitedBanner", "StandardBanner"]  # 支持的卡池种类，请勿修改
+banner_to_simulate = banner_types[0]  # 选择模拟的卡池
+
+sample = 100000
+n = 1  # number of main operators
+m = 0  # number of peipao operators
+###########################################
+
+
+def export_to_csv(new_column: list, column_name: str, file_path: str = "sample_output.csv") -> None:
     """Add or create a column in an existing CSV file."""
     try:
-        df = pd.read_csv(file_name)
+        df = pd.read_csv(file_path)
         df[column_name] = new_column
     except FileNotFoundError:
         df = pd.DataFrame({column_name: new_column})
 
-    df.to_csv(file_name, index=False)
-    print(f"Column '{column_name}' exported to '{file_name}'")
+    df.to_csv(file_path, index=False)
+    print(f"Column '{column_name}' exported to '{file_path}'")
 
 
-def summarize(input_list: list[int]) -> dict:
-    """Given an input_list, output its min, max, mean, median, and key percentiles as rounded integers."""
-    data = np.array(input_list)
-    rslt = {
-        "min": round(np.min(data)),
-        "max": round(np.max(data)),
-        "mean": round(np.mean(data)),
-        "median": round(np.median(data)),
-        "p10": round(np.percentile(data, 10)),
-        "p25": round(np.percentile(data, 25)),
-        "p75": round(np.percentile(data, 75)),
-        "p90": round(np.percentile(data, 90))
-    }
-
-    for key, value in rslt.items():
-        print(f"{key}: {value}")
-    return rslt
-
-
-def plot_histogram(input_list: list[int]) -> None:
-    """Create a histogram about input_list.
-    """
-    plt.hist(input_list, bins = 30)
-    plt.xlabel("Number of pulls")
-    plt.ylabel("Count")
-    plt.show()
-
-
-def find_percentile(input_list: list[int], n: int) -> float:
-    """Find the percentile of n in input_list."""
-    cnt = 0
-    for item in input_list:
-        if item < n:
-            cnt += 1
-    return cnt / len(input_list)
-
-
-def simulate(banner_type: str, n_sample: int, n_rate_up: int, m_rate_up: int = 0,
-             to_csv: bool = False, file_name: str = "output.csv") -> list:
+def simulate(banner_type: str, num_sample: int, n_rate_up: int, m_rate_up: int = 0,
+             to_csv: bool = False, file_path: str = "sample_output.csv") -> list:
     """Make simulations with banner_type banner, aim to get n_rate_up main
     operator(s) and m_rate_up peipao operator(s) if any. If sort = True, the
     result will be sorted in ascending order. When to_csv = True, results will
-    be exported to file_name.
+    be exported to file_path.
     """
-    types = ["Special banner", "Limited banner", "Standard banner"]
-    if banner_type not in types:
+    if banner_type not in banner_types:
         raise ValueError(f"Banner type {banner_type} does not exist.")
     if n_rate_up == 0 and m_rate_up == 0:
         raise ValueError("Does not apply: please get at least 1 "
                          "main/peipao operator.")
 
     # Initialize the banner.
-    if banner_type == "Special banner":
+    if banner_type == banner_types[0]:
         b = SpecialBanner()
-        print(f"开始模拟「特殊寻访」，抽取「{n_rate_up}」个up干员，样本量：{n_sample}")
-    elif banner_type == "Limited banner":
+        print(f"开始模拟「特殊寻访」，抽取「{n_rate_up}」个up干员，样本量：{num_sample}")
+    elif banner_type == banner_types[1]:
         b = LimitedBanner()
-        print(f"开始模拟「限定寻访」，抽取至少「{n_rate_up}」个限定干员、「{m_rate_up}」个陪跑干员，样本量：{n_sample}")
-    elif banner_type == "Standard banner":
+        print(f"开始模拟「限定寻访」，抽取至少「{n_rate_up}」个限定干员、「{m_rate_up}」个陪跑干员，样本量：{num_sample}")
+    elif banner_type == banner_types[2]:
         b = StandardBanner()
-        print(f"开始模拟「标准寻访」，抽取至少「{n_rate_up}」个进店干员、「{m_rate_up}」个陪跑干员，样本量：{n_sample}")
+        print(f"开始模拟「标准寻访」，抽取至少「{n_rate_up}」个进店干员、「{m_rate_up}」个陪跑干员，样本量：{num_sample}")
+    else:
+        raise ValueError(f"模拟的卡池{b}不支持")
 
     # Simulate
     results = []
     t0 = time.time()
-    for _ in trange(n_sample):
+    for _ in trange(num_sample):
         if isinstance(b, SpecialBanner):
             results.append(b.pull_n_desired(n_rate_up)[-1])
         else:
@@ -109,21 +84,15 @@ def simulate(banner_type: str, n_sample: int, n_rate_up: int, m_rate_up: int = 0
         else:
             col_name = f'{type(b).__name__}: {n_rate_up}+{m_rate_up}'
         print(col_name)
-        export_to_csv(results, col_name, file_name)
+        export_to_csv(results, col_name, file_path)
 
     return results
 
+
 if __name__ == '__main__':
-    ############### Basic Setup ###############
-    random.seed(123)
-    banner_types = ["Special banner", "Limited banner", "Standard banner"]
-
-    sample = 100000
-    n = 6  # number of main operators
-    m = 0  # number of peipao operators
-    ###########################################
-
-    result = simulate(banner_types[0], sample, n, m)
+    result = simulate(banner_type=banner_to_simulate, num_sample=sample,
+                      n_rate_up=n, m_rate_up=m,
+                      to_csv=True, file_path=f"simulations/{banner_to_simulate}.csv")
 
     print()
     n_pulls = 46
@@ -134,5 +103,5 @@ if __name__ == '__main__':
     # plot_histogram(result)
 
     # for i in range(6):
-    #     result = simulate(banner_types[1], sample, i+1, to_csv = False, file_name="limited_banner.csv")
+    #     result = simulate(banner_types[1], sample, i+1, to_csv = False, file_path="limited_banner.csv")
     #     summarize(result)
